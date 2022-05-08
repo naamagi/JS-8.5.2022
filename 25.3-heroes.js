5af45cfe69289c86338bfe90389e86400c670966
naamagi <n.gilat@gmail.com>
2022-05-08 19:25:05 +0300

    update

---
+++ 25.3-heroes.js
@@ -0,0 +1,23 @@
+
+const wonderWoman = {
+  name: "Diana Prince",
+};
+const batman = {
+  name: "Bruce Wayne",
+};
+const superHeroes = [wonderWoman, batman];
+
+
+function printName() {
+  console.log(`my name is ${this.name}`);
+}
+
+
+function printHeroes(heroes, printFunc) {
+  for (const hero of heroes) {
+    printFunc.call(hero);
+  }
+}
+
+
+printHeroes(superHeroes,printName);
